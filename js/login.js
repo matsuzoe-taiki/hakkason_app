64cd59f7cdf7d418808040affeab2e34f9968dba
matsuzoe-taiki <matsutaiyaki1013@yahoo.co.jp>
2026-02-01 13:35:57 +0900

    ログインページ完成

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// ログイン画面のJS処理
+const userInput = document.getElementById('userInput');
+const startButton = document.getElementById('startButton');
+
+startButton.addEventListener("click", () => {
+    const username = userInput.value;
+    console.log(username);
+    localStorage.setItem("username", username);
+    alert("データ保存完了");
+});
+window.addEventListener("DOMContentLoaded", () => {
+    const saved = localStorage.getItem("username");
+    if (saved) {
+        window.location.href = 'http://localhost:8888/hakkason_app/pages/home.html';
+    }
+})
